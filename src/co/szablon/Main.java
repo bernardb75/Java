15d14811f90b0965f4f7ef988c3b4d37f88dc7f8
Epatriot <35653568+bernardb75@users.noreply.github.com>
2026-07-01 19:37:02 +0200

    Delete src directory

--- src/co/szablon/Main.java
+++
@@ -1,8 +0,0 @@
-package co.szablon;
-
-public class Main {
-public static void main(String[] args) {
-	MetodaSzablonowa myDay=new MyDay();
-	myDay.dzienPracy(TransportType.TRAM);
-}
-}
--- src/co/szablon/TransportType.java
+++
@@ -1,5 +0,0 @@
-package co.szablon;
-
-public enum TransportType {
-CAR,TRAM,BIKE
-}
